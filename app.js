a83155a047f29a993b72a65b9794ff71d1f974c9
Sina E <sinaizad@gmail.com>
2020-04-23 17:47:55 -0400

    fix port issue

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 80
+const PORT = process.env.PORT || 5000
 
 app.use( express.static('public'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
